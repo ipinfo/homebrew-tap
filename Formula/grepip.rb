431ec996b863413002537e3f983930b47a52904a
Uman Shahzad <uman@mslm.io>
2021-05-16 15:35:52 +0500

    update grepip to 1.0.2 and add more tests

--- Formula/grepip.rb
+++ Formula/grepip.rb
@@ -1,8 +1,8 @@
 class Grepip < Formula
   desc "Filters IPv4 & IPv6 addresses with a grep-compatible interface"
   homepage "https://ipinfo.io"
-  url "https://github.com/ipinfo/cli/archive/grepip-1.0.1.tar.gz"
-  sha256 "5e03a5ea3ce631daa4d616809a035c440c11ef5cb69bbd161e067d56c6dcb402"
+  url "https://github.com/ipinfo/cli/archive/grepip-1.0.2.tar.gz"
+  sha256 "f9258f9f5c2bfdb4278c6cd456072664be7588fc6a8b665d35cbd6fa29f57568"
   license "Apache-2.0"
 
   depends_on "go" => :build
@@ -14,6 +14,15 @@ class Grepip < Formula
 
   test do
     assert_equal version.to_s, shell_output("#{bin}/grepip --version").chomp
-    assert_equal "1.1.1.1\n", `echo "asdf 1.1.1.1 asdf" | #{bin}/grepip -o`
+    assert_equal "1.1.1.1", pipe_output("#{bin}/grepip -o", "asdf 1.1.1.1 asdf").chomp
+
+    (testpath/"access.log").write <<~EOS
+      127.0.0.1 valid ip but reserved
+      111.119.187.44 valid ip
+      8.8.8. invalid ip
+      no ip
+    EOS
+    output = shell_output("#{bin}/grepip --exclude-reserved -h access.log")
+    assert_equal "111.119.187.44 valid ip", output.strip
   end
 end
